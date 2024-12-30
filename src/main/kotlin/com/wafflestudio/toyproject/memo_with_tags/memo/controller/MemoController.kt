678baa2de0b043654bf1cbe629c17dc0f6f2cff7
Leafguyk <81000589+Leafguyk@users.noreply.github.com>
2024-12-30 21:08:59 +0900

    Controller Skeleton (#5)
    Co-authored-by: 김인호 <79935974+kih00@users.noreply.github.com>

--- src/main/kotlin/com/wafflestudio/toyproject/memo_with_tags/memo/controller/MemoController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memo_with_tags/memo/controller/MemoController.kt
@@ -7,13 +7,39 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MenuController(
+class MemoController(
     private val memoService: MemoService
 ) {
+    @PostMapping("/api/v1/memo")
+    fun createMemo(@RequestBody request: CreateMemoRequest): MemoDto {
+    }
 
+    @PutMapping("/api/v1/memo/{memoId}")
+    fun updateMemo(@PathVariable memoId: Long, @RequestBody request: UpdateMemoRequest): MemoDto {
+    }
+
+    @DeleteMapping("/api/v1/memo/{memoId}")
+    fun deleteMemo(@PathVariable memoId: Long) {
+    }
 }
 
-enum class SortType {
-    ByRating,
-    ByNumberOfReviews,
-}
+data class MemoDto(
+    val content: String,
+    val tags: List<Long>
+)
+
+data class CreateMemoResponse(
+    val id: Long,
+    val content: String,
+    val tags: List<Long>,
+    val createdAt: Instant,
+    val updatedAt: Instant
+)
+
+data class UpdateMemoResponse(
+    val id: Long,
+    val content: String,
+    val tags: List<Long>,
+    val createdAt: Instant,
+    val updatedAt: Instant
+)
