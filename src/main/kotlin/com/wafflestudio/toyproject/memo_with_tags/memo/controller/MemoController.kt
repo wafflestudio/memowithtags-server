a803e22d74f78fd9dcaa32609e68d4ccc9168554
Leafguyk <81000589+Leafguyk@users.noreply.github.com>
2024-12-30 20:59:12 +0900

    Controller Skeleton
    Controller Skeleton Update

--- src/main/kotlin/com/wafflestudio/toyproject/memo_with_tags/memo/controller/MemoController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memo_with_tags/memo/controller/MemoController.kt
@@ -1,2 +1,44 @@
 package com.wafflestudio.toyproject.memo_with_tags.memo.controller
 
+@RestController
+class MemoController(
+    private val memoService: MemoService
+) {
+    @PostMapping("/api/v1/memo")
+    fun createMemo(@RequestBody request: CreateMemoRequest): CreateMemoResponse {
+    }
+
+    @PutMapping("/api/v1/memo/{memoId}")
+    fun updateMemo(@PathVariable memoId: Long, @RequestBody request: UpdateMemoRequest): UpdateMemoResponse {
+    }
+
+    @DeleteMapping("/api/v1/memo/{memoId}")
+    fun deleteMemo(@PathVariable memoId: Long) {
+    }
+}
+
+data class CreateMemoRequest(
+    val content: String,
+    val tags: List<Long>
+)
+
+data class CreateMemoResponse(
+    val id: Long,
+    val content: String,
+    val tags: List<Long>,
+    val createdAt: Instant,
+    val updatedAt: Instant
+)
+
+data class UpdateMemoRequest(
+    val content: String,
+    val tags: List<Long>
+)
+
+data class UpdateMemoResponse(
+    val id: Long,
+    val content: String,
+    val tags: List<Long>,
+    val createdAt: Instant,
+    val updatedAt: Instant
+)
--- src/main/kotlin/com/wafflestudio/toyproject/memo_with_tags/user/contoller/UserController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memo_with_tags/user/contoller/UserController.kt
@@ -1,2 +1,88 @@
 package com.wafflestudio.toyproject.memo_with_tags.user.contoller
 
+@RestController
+class UserController(
+
+) {
+    @GetMapping("/api/v1/auth/register")
+    fun register(@RequestBody request: RegisterRequest): RegisterResponse {
+
+    }
+
+    @PostMapping("/api/v1/auth/login")
+    fun login(@RequestBody request: LoginRequest): LoginResponse {
+
+    }
+
+    @PostMapping("/api/v1/auth/logout")
+    fun logout(@RequestBody request: LogoutRequest) {
+
+    }
+
+    @PostMapping("/api/v1/auth/verify-email")
+    fun verifyEmail(@RequestBody request: VerifyEmailRequest) {
+
+    }
+
+    @PostMapping("/api/v1/auth/forgot-password")
+    fun forgotPassword(@RequestBody request: ForgotPasswordRequest) {
+
+    }
+
+    @PostMapping("/api/v1/auth/reset-password")
+    fun resetPassword(@RequestBody request: ResetPasswordRequest) {
+
+    }
+
+    @PostMapping("/api/v1/auth/refresh-token")
+    fun refreshToken(): RefreshTokenResponse {
+
+    }
+
+}
+
+data class RegisterRequest(
+    val email: String,
+    val password: String
+)
+
+data class RegisterResponse(
+    val accessToken: String,
+    val refreshToken: String,
+    val expiresIn: Long
+)
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+data class LoginResponse(
+    val accessToken: String,
+    val refreshToken: String,
+    val expiresIn: Long
+)
+
+data class LogoutRequest(
+    val Authorization: String,
+    val refreshToken: String
+)
+
+data class VerifyEmailRequest(
+    val email: String
+)
+
+data class ForgotPasswordRequest(
+    val email: String
+)
+
+data class ResetPasswordRequest(
+    val email: String,
+    val password: String
+)
+
+data class RefreshTokenResponse(
+    val accessToken: String,
+    val refreshToken: String,
+    val expiresIn: Long
+)
