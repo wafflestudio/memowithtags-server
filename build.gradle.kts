9705690834f7ebf1229055d23223c71c1ebcd067
Yongbeom Kim <kybs0627@snu.ac.kr>
2025-01-02 17:08:07 +0900

    actions변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id ("org.jlleitschuh.gradle.ktlint") version "11.5.1"
 	kotlin("plugin.jpa") version "1.9.25"
 }
 
