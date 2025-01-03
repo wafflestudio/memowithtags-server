21ce41b78c270673b2498e2d72ffc38ab88d9a96
Yongbeom Kim <kybs0627@snu.ac.kr>
2025-01-03 21:36:00 +0900

    actions변경

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/JwtUtil.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/JwtUtil.kt
@@ -4,15 +4,15 @@ import io.jsonwebtoken.security.Keys
 import java.util.Date
 import javax.crypto.SecretKey
 
-object UserAccessTokenUtil {
+object JwtUtil {
     private val secretKey: SecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256)
     private const val ACCESS_TOKEN_EXPIRATION: Long = 1000 * 60 * 120 // 2시간
     private const val REFRESH_TOKEN_EXPIRATION: Long = 1000 * 60 * 60 * 24 * 14 // 14일
 
     // Access Token 생성
-    fun generateAccessToken(username: String): String {
+    fun generateAccessToken(userId: String): String {
         return Jwts.builder()
-            .setSubject(username)
+            .setSubject(userId)
             .setIssuedAt(Date())
             .setExpiration(Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION))
             .signWith(secretKey)
@@ -20,9 +20,9 @@ object UserAccessTokenUtil {
     }
 
     // Refresh Token 생성
-    fun generateRefreshToken(username: String): String {
+    fun generateRefreshToken(userId: String): String {
         return Jwts.builder()
-            .setSubject(username)
+            .setSubject(userId)
             .setIssuedAt(Date())
             .setExpiration(Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION))
             .signWith(secretKey)
