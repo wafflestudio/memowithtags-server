229461555e13f18c7f75107ee63d57e355040219
Yongbeom Kim <96988554+Criminal0627@users.noreply.github.com>
2025-01-04 21:59:49 +0900

    feature/user (#18)
    Co-authored-by: kih02 <inho5248@snu.ac.kr>

---
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/contoller/User.kt
@@ -0,0 +1,19 @@
+package com.wafflestudio.toyproject.memoWithTags.user.contoller
+
+import com.wafflestudio.toyproject.memoWithTags.user.persistence.UserEntity
+
+class User(
+    val id: Long,
+    val email: String,
+    val nickname: String
+) {
+    companion object {
+        fun fromEntity(entity: UserEntity): User {
+            return User(
+                id = entity.id!!,
+                email = entity.email,
+                nickname = entity.nickname
+            )
+        }
+    }
+}
--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
@@ -1,3 +1,20 @@
 package com.wafflestudio.toyproject.memoWithTags.user.persistence
 
-class UserEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity(name = "users")
+class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(name = "email", nullable = false)
+    var email: String,
+    @Column(name = "nickname", nullable = false)
+    var nickname: String = "Writer",
+    @Column(name = "hashed_password", nullable = false)
+    var hashedPassword: String
+)
