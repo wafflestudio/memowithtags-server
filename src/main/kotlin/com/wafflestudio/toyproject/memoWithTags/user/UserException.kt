058c1fe10b8ddf56f265819637ea96dc9be7520b
kih00 <inho524890@gmail.com>
2025-01-05 04:15:03 +0900

    add mail sending feature, userservice와 usercontroller 더 수정

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/UserException.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/UserException.kt
@@ -3,4 +3,9 @@ package com.wafflestudio.toyproject.memoWithTags.user
 sealed class UserException : RuntimeException()
 
 class EmailNotFoundException : UserException()
+
+class EmailVerificationFailureException : UserException()
+
+class SignInInvalidPasswordException : UserException()
+
 class AuthenticationFailedException : UserException()
--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import java.time.Instant
 
 @Entity(name = "users")
 class UserEntity(
@@ -12,9 +13,13 @@ class UserEntity(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     @Column(name = "email", nullable = false)
-    var email: String,
+    val email: String,
     @Column(name = "nickname", nullable = false)
     var nickname: String = "Writer",
     @Column(name = "hashed_password", nullable = false)
-    var hashedPassword: String
+    var hashedPassword: String,
+    @Column(name = "verified", nullable = false)
+    var verified: Boolean = false,
+    @Column(name = "created_at", nullable = false)
+    val createdAt: Instant
 )
