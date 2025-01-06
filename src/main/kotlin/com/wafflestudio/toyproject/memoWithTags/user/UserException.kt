c24c859055c2bb0d6833f7efc6dc46784cc8bd98
Yongbeom Kim <kybs0627@snu.ac.kr>
2025-01-06 20:25:09 +0900

    전체적으로 수정

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/UserException.kt
+++
@@ -1,6 +0,0 @@
-package com.wafflestudio.toyproject.memoWithTags.user
-
-sealed class UserException : RuntimeException()
-
-class EmailNotFoundException : UserException()
-class AuthenticationFailedException : UserException()
