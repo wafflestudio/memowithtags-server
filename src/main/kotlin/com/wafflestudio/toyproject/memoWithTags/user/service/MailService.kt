89f7d57ed1a7c81533ff4f23e7ca17573ceec272
김인호 <79935974+kih00@users.noreply.github.com>
2025-01-15 19:47:59 +0900

    Feature/verify email (#47)

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/service/MailService.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/service/MailService.kt
@@ -1,33 +1,5 @@
 package com.wafflestudio.toyproject.memoWithTags.user.service
 
-import jakarta.mail.MessagingException
-import jakarta.mail.internet.MimeMessage
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.MimeMessageHelper
-import org.springframework.stereotype.Service
-
-@Service
-class MailService(
-    private val mailSender: JavaMailSender,
-    @Value("\${spring.mail.username}")
-    private val fromMail: String
-) {
-
-    fun sendEmail(toEmail: String, title: String, content: String) {
-        val message: MimeMessage = mailSender.createMimeMessage()
-        val helper = MimeMessageHelper(message, true, "UTF-8")
-        try {
-            helper.apply {
-                setFrom(fromMail)
-                setTo(toEmail)
-                setSubject(title)
-                setText(content, true)
-            }
-            mailSender.send(message)
-        } catch (e: MessagingException) {
-            e.printStackTrace()
-            throw RuntimeException("unable to send email", e)
-        }
-    }
+interface MailService {
+    fun sendMail(toEmail: String, title: String, content: String)
 }
