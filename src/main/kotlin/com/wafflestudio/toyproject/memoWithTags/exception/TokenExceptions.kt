6532701d80ad51f1bd0f1af8567f8350d308c059
kih00 <inho524890@gmail.com>
2025-01-19 01:43:42 +0900

    lint 수정

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/exception/TokenExceptions.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/exception/TokenExceptions.kt
@@ -1,19 +1,26 @@
 package com.wafflestudio.toyproject.memoWithTags.exception
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 
-class TokenExpiredException : CustomException(
+sealed class TokenExceptions(
+    httpErrorCode: HttpStatusCode,
+    errorCode: Int,
+    msg: String
+) : CustomException(httpErrorCode, errorCode, msg)
+
+class TokenExpiredException : TokenExceptions(
     errorCode = 0,
     httpErrorCode = HttpStatus.UNAUTHORIZED,
     msg = "토큰이 만료되었습니다."
 )
 
-class InvalidTokenSignatureException : CustomException(
+class InvalidTokenSignatureException : TokenExceptions(
     errorCode = 0,
     httpErrorCode = HttpStatus.UNAUTHORIZED,
     msg = "토큰의 서명이 유효하지 않습니다."
 )
 
-class InValidTokenException : CustomException(
+class InValidTokenException : TokenExceptions(
     errorCode = 0,
     httpErrorCode = HttpStatus.UNAUTHORIZED,
     msg = "토큰이 유효하지 않습니다."
