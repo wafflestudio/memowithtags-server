da41e15c10d177734fa987841b60e08b54abdd81
Leafguyk <leafguyk@gmail.com>
2025-01-19 18:47:26 +0900

    네이버 소셜로그인

---
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/dto/NaverUser.kt
@@ -0,0 +1,20 @@
+package com.wafflestudio.toyproject.memoWithTags.user.dto
+
+data class NaverOAuthToken(
+    val token_type: String,
+    val access_token: String,
+    val expires_in: Int,
+    val refresh_token: String
+)
+
+data class NaverProfileResponse(
+    val resultcode: String,
+    val message: String,
+    val response: NaverProfile
+)
+
+data class NaverProfile(
+    val id: String,
+    val nickname: String,
+    val email: String
+)
