f230009f2846d55c66eb48adfa413236e0b04bb9
Leafguyk <leafguyk@gmail.com>
2025-01-21 16:30:13 +0900

    Update AdminController.kt

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/controller/AdminController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/controller/AdminController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -47,7 +48,7 @@ class AdminController(
     }
 
     @Operation(summary = "유저 계정 업데이트")
-    @GetMapping("/admin/user")
+    @PutMapping("/admin/user")
     fun getUsers(@AuthUser user: User, @RequestBody request: UpdateUserRequest): User {
         adminService.isAdmin(user.id)
         return adminService.updateUser(request.id, request.userUpdateInfo)
