10553d0131a84a643845d2750d63241faa909594
kih00 <inho524890@gmail.com>
2025-01-21 23:18:29 +0900

    로그인 리다이렉트 수정

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/controller/SocialLoginController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/controller/SocialLoginController.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.toyproject.memoWithTags.user.controller
 import com.wafflestudio.toyproject.memoWithTags.exception.OAuthRequestException
 import com.wafflestudio.toyproject.memoWithTags.user.dto.UserResponse.LoginResponse
 import com.wafflestudio.toyproject.memoWithTags.user.service.SocialLoginService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -31,6 +32,14 @@ class SocialLoginController(
 
     @GetMapping("/oauth/kakao")
     fun kakaoCallback(
+        @RequestParam("code") code: String
+    ): ResponseEntity<Unit> {
+        val appLink = "memowithtags://oauth/kakao?code=$code"
+        return ResponseEntity.status(HttpStatus.FOUND).build()
+    }
+
+    @GetMapping("/oauth/kakao/login")
+    fun kakaoLogin(
         @RequestParam("code") code: String
     ): ResponseEntity<LoginResponse> {
         val (_, accessToken, refreshToken) = socialLoginService.kakaoCallBack(code)
--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
@@ -3,7 +3,6 @@ package com.wafflestudio.toyproject.memoWithTags.user.persistence
 import com.wafflestudio.toyproject.memoWithTags.memo.persistence.MemoEntity
 import com.wafflestudio.toyproject.memoWithTags.tag.persistence.TagEntity
 import com.wafflestudio.toyproject.memoWithTags.user.RoleType
-import com.wafflestudio.toyproject.memoWithTags.user.RoleType.ROLE_USER
 import com.wafflestudio.toyproject.memoWithTags.user.SocialType
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
@@ -29,7 +28,7 @@ class UserEntity(
     @Column(name = "verified", nullable = false)
     var verified: Boolean = false,
     @Column(name = "role", nullable = false)
-    var role: RoleType = ROLE_USER,
+    var role: RoleType = RoleType.ROLE_USER,
     @Column(name = "social_type", nullable = true)
     var socialType: SocialType? = null,
     @Column(name = "created_at", nullable = false)
