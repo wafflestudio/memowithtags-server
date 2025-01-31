81e67069fe3401b58df5241095c3f2ee9e0a3aa8
kih00 <inho524890@gmail.com>
2025-01-31 15:50:22 +0900

    user 필드에 식별 번호 추가, withdrawal API 추가

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/persistence/UserEntity.kt
@@ -19,22 +19,34 @@ class UserEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     val id: UUID? = null,
+
+    @Column(name = "user_number", nullable = false)
+    val userNumber: Int,
+
     @Column(name = "email", nullable = false)
     val email: String,
+
     @Column(name = "nickname", nullable = false)
     var nickname: String = "Writer",
+
     @Column(name = "hashed_password", nullable = false)
     var hashedPassword: String,
+
     @Column(name = "verified", nullable = false)
     var verified: Boolean = false,
+
     @Column(name = "role", nullable = false)
     var role: RoleType = RoleType.ROLE_USER,
+
     @Column(name = "social_type", nullable = true)
     var socialType: SocialType? = null,
+
     @Column(name = "created_at", nullable = false)
     val createdAt: Instant,
+
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
     var memos: MutableList<MemoEntity> = mutableListOf(),
+
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
     var tags: MutableList<TagEntity> = mutableListOf()
 )
