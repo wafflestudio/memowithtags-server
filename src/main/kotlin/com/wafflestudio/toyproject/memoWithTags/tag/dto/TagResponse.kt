0208588a34f5cf5f386db8b15527f1bc581a3bc4
Leafguyk <81000589+Leafguyk@users.noreply.github.com>
2025-01-31 12:20:21 +0900

    Tag Id changed to UUID

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/tag/dto/TagResponse.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/tag/dto/TagResponse.kt
@@ -1,8 +1,4 @@
 package com.wafflestudio.toyproject.memoWithTags.tag.dto
 
 sealed class TagResponse {
-    data class UpdateTagRequest(
-        val name: String,
-        val colorHex: String
-    ) : TagResponse()
 }
