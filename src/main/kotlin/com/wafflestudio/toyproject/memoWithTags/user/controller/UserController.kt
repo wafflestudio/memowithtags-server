2a9a9e7928ef06449882a1df3598f22967a6e6fb
kih00 <inho524890@gmail.com>
2025-02-22 22:16:06 +0900

    메일 인증 API를 두 단계로 분리 및 수정

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/controller/UserController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/user/controller/UserController.kt
@@ -13,7 +13,6 @@ import com.wafflestudio.toyproject.memoWithTags.user.dto.UserResponse.RefreshTok
 import com.wafflestudio.toyproject.memoWithTags.user.service.UserService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
