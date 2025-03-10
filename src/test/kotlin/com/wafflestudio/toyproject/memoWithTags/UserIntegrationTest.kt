56405967af4dc337ca1b6d87615f66ca54efd934
Yongbeom Kim <kybs0627@snu.ac.kr>
2025-03-10 20:57:03 +0900

    tagTest변경

--- src/test/kotlin/com/wafflestudio/toyproject/memoWithTags/UserIntegrationTest.kt
+++ src/test/kotlin/com/wafflestudio/toyproject/memoWithTags/UserIntegrationTest.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPat
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.transaction.annotation.Transactional
 
+@ActiveProfiles("dev")
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
