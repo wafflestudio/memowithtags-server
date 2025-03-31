60d31cdce32602edb5073134fc450defe2b2b643
Leafguyk <leafguyk@gmail.com>
2025-03-31 20:02:29 +0900

    RequestBody 형식에 맞지 않은 요청 보낼 때 Response 수정

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/exception/handler/GlobalExceptionHandler.kt
@@ -1,9 +1,13 @@
 package com.wafflestudio.toyproject.memoWithTags.exception.handler
 
+import com.wafflestudio.toyproject.memoWithTags.exception.code.ErrorCode
 import com.wafflestudio.toyproject.memoWithTags.exception.exceptions.CustomException
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -11,5 +15,12 @@ class GlobalExceptionHandler {
     fun handleCustomException(ex: CustomException): ResponseEntity<ErrorResponse> {
         val errorResponse = ErrorResponse(ex.errorCode)
         return ResponseEntity.status(ex.errorCode.status).body(errorResponse)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleValidationException(e: HttpMessageNotReadableException?): ResponseEntity<ErrorResponse> {
+        val error = ErrorCode.SERVER_HTTP_MESSAGE_NOT_READABLE
+        val errorResponse = ErrorResponse(error)
+        return ResponseEntity.status(error.status).body(errorResponse)
     }
 }
