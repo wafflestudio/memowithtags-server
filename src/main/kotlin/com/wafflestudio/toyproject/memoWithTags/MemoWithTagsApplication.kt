9f20a65cf08d45eeac6fe123f7612d975def0b4f
kih00 <inho524890@gmail.com>
2025-04-02 00:58:40 +0900

    메일 인증번호 TTL 적용 오류 수정 중

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/MemoWithTagsApplication.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/MemoWithTagsApplication.kt
@@ -2,8 +2,10 @@ package com.wafflestudio.toyproject.memoWithTags
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableRedisRepositories
 @SpringBootApplication
 @EnableScheduling
 class MemoWithTagsApplication
