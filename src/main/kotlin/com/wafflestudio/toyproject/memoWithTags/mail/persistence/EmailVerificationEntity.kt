b2cbaf1414e843b8147af301807ca644caf96bf0
kih00 <inho524890@gmail.com>
2025-04-04 13:33:10 +0900

    redis TTL 문제 관련 수정

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/mail/persistence/EmailVerificationEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/mail/persistence/EmailVerificationEntity.kt
@@ -3,7 +3,6 @@ package com.wafflestudio.toyproject.memoWithTags.mail.persistence
 import jakarta.persistence.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
-import org.springframework.data.redis.core.index.Indexed
 
 @RedisHash(value = "email_verification")
 data class EmailVerificationEntity(
