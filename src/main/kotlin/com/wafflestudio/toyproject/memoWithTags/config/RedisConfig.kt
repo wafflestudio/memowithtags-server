359fcffc5fb1caa2d8640de495aee1fb08631300
Kim Inho <79935974+kih00@users.noreply.github.com>
2025-07-21 21:19:15 +0900

    redis secret 주입 문제 해결 (#139)

--- src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/config/RedisConfig.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/memoWithTags/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
@@ -12,10 +13,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 @Configuration
 class RedisConfig(
     @Value("\${spring.data.redis.host}") private val host: String,
-    @Value("\${spring.data.redis.port}") private val port: Int
+    @Value("\${spring.data.redis.database}") private val database: Int
 ) {
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(host, port)
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val config = RedisStandaloneConfiguration()
+        config.hostName = host
+        config.port = 6379
+        config.database = database
+        return LettuceConnectionFactory(config)
+    }
 
     @Bean
     fun redisTemplate(factory: RedisConnectionFactory): RedisTemplate<String, Any> {
